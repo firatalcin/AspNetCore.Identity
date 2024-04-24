be8dcc08dfd58028da01282cec87fcef32b4e970
Fırat Alçın <firatalcin04@gmail.com>
2024-04-24 15:22:54 +0300

    Sign Up Sayfası Tasarlandı

---
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+        [Display(Name = "Kullanıcı Adı: ")]
+        public string? UserName { get; set; }
+        [Display(Name = "Email: ")]
+        public string? Email { get; set; }
+        [Display(Name = "Telefon: ")]
+        public string? Phone { get; set; }
+        [Display(Name = "Şifre: ")]
+        public string? Password { get; set; }
+        [Display(Name = "Şifre Tekrar: ")]
+        public string? PasswordConfirm { get; set; }
+    }
+}
