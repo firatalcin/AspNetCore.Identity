b7b9751c3b5e029e1c181bd84144873c81b489de
Fırat Alçın <firatalcin04@gmail.com>
2024-08-19 23:19:50 +0300

    SignIn Sayfasının Kodlanması

--- AspNetCoreIdentityApp/AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using AspNetCoreIdentityApp.Web.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using AspNetCoreIdentityApp.Web.Extensions;
 
 namespace AspNetCoreIdentityApp.Web.Controllers
 {
@@ -10,11 +11,13 @@ namespace AspNetCoreIdentityApp.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _logger = logger;
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -58,16 +61,40 @@ namespace AspNetCoreIdentityApp.Web.Controllers
                 return RedirectToAction(nameof(HomeController.SignUp));
             }
 
-            foreach (IdentityError item in identityResult.Errors)
-            {
-                ModelState.AddModelError(string.Empty, item.Description);
-            }
+            ModelState.AddModelErrorList(identityResult.Errors.Select(x => x.Description).ToList());
+
             return View();
         }
 
         [HttpGet]
         public IActionResult SignIn()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel model, string? returnUrl=null)
+        {
+            returnUrl = returnUrl != null ? returnUrl : Url.Action("Index", "Home");
+
+            var hasUser = await _userManager.FindByEmailAsync(model.Email);
+
+            if (hasUser == null)
+            {
+                ModelState.AddModelError(string.Empty, "Email veya şifre yanlış");
+                return View();
+            }
+
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser, model.Password, model.RememberMe, false);
+
+            if (signInResult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Başarıyla Giriş Yaptınız";
+                return Redirect(returnUrl);
+            }
+
+            ModelState.AddModelErrorList(new List<string>() { "Email veya Şifre yanlış" });            
+
             return View();
         }
 
---
+++ AspNetCoreIdentityApp/AspNetCoreIdentityApp.Web/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AspNetCoreIdentityApp.Web.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        public static void AddModelErrorList(this ModelStateDictionary modelState, List<string> errors)
+        {
+            foreach (var item in errors)
+            {
+                modelState.AddModelError(string.Empty, item);
+            }
+        }
+    }
+}
