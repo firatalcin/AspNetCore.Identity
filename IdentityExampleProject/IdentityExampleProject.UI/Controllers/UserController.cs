a42444942e0a3760b0761f8db28e148547db2fcd
Fırat Alçın <firatalcin04@gmail.com>
2024-09-18 00:31:13 +0300

    UserManager

---
+++ IdentityExampleProject/IdentityExampleProject.UI/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using IdentityExampleProject.UI.Models.Authentication;
+using IdentityExampleProject.UI.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityExampleProject.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(AppUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    Country = "Istanbul"
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(appUser, model.Sifre);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "User");   
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ IdentityExampleProject/IdentityExampleProject.UI/ViewModels/AppUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityExampleProject.UI.ViewModels
+{
+    public class AppUserViewModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adını boş geçmeyiniz...")]
+        [StringLength(15, ErrorMessage = "Lütfen kullanıcı adını 4 ile 15 karakter arasında giriniz...", MinimumLength = 4)]
+        [Display(Name = "Kullanıcı Adı")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Lütfen emaili boş geçmeyiniz...")]
+        [EmailAddress(ErrorMessage = "Lütfen email formatında bir değer belirtiniz...")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Lütfen şifreyi boş geçmeyiniz...")]
+        [DataType(DataType.Password, ErrorMessage = "Lütfen şifreyi tüm kuralları göz önüne alarak giriniz...")]
+        [Display(Name = "Şifre")]
+        public string Sifre { get; set; }
+    }
+}
